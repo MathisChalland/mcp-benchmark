35c6b4149964387ac0056b68419ea00cce04bfdf
Mathis <mtb.mathis@gmail.com>
2026-01-16 15:27:03 +0100

    init

---
+++ src/server/better-auth/config.ts
@@ -0,0 +1,28 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+import { env } from "@/env";
+import { db } from "@/server/db";
+
+export const auth = betterAuth({
+  database: prismaAdapter(db, {
+    provider: "postgresql", // or "sqlite" or "mysql"
+  }),
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 60, // Cache duration in seconds
+    },
+    expiresIn: 60 * 60 * 24 * 14, // 14 days
+    updateAge: 60 * 60 * 24, // 1 day (every 1 day the session expiration is updated)
+  },
+  socialProviders: {
+    github: {
+      clientId: env.BETTER_AUTH_GITHUB_CLIENT_ID,
+      clientSecret: env.BETTER_AUTH_GITHUB_CLIENT_SECRET,
+      redirectURI: "http://localhost:3000/api/auth/callback/github",
+    },
+  },
+});
+
+export type Session = typeof auth.$Infer.Session;
